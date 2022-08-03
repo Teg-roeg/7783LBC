e0393a1f140540cddb47ee8589b3f491354ad24f
Tegroeg <mr.gosha8@gmail.com>
2022-08-03 16:16:29 +0100

    blah blah

---
+++ index.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+        }
+    }
+}
